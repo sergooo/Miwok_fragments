1081a5c8672b10c0864b1d0721d6a487627b8ee2
mobilotest <mobilotest@gmail.com>
2020-03-31 23:52:15 -0700

    added a lot of new features

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -1,7 +1,5 @@
 package com.example.android.miwok;
 
-import android.media.Image;
-
 /**
  * {@link Word} represents a vocabulary word that the user wants to learn.
  * It contains a default translation and a Miwok translation for that word.
@@ -14,13 +12,27 @@ public class Word {
     /** Miwok translation for the word */
     private String mMiwokTranslation;
 
+    /** Miwok image for each word */
+    private int mImageResourceId;
+
+    /** Constant value that represents no image was provided for this word */
+    private static final int NO_IMAGE_PROVIDED = -1;
+
     /**
      * Create a new Word object.
      *
      * @param defaultTranslation is the word in a language that the user is already familiar with
      *                           (such as English)
      * @param miwokTranslation is the word in the Miwok language
+     *
+     * @param imageResourceId is the images for each word
      */
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
+    }
+
     public Word(String defaultTranslation, String miwokTranslation) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
@@ -40,11 +52,17 @@ public class Word {
         return mMiwokTranslation;
     }
 
-//    /**
-//     * Get the Miwok image of the both words.
-//     */
-//    public Image getMiwokNumImage() {
-//        return mMiwokTranslation;
-//    }
+    /**
+     * Get the image for each word.
+     */
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
 
+    /**
+     * Returns whether or not there is an image for this word.
+     */
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,18 +1,29 @@
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
 
-public class WordAdapter extends ArrayAdapter<Word>  {
-    public WordAdapter(NumbersActivity numbersActivity, ArrayList<Word> words) {
-        super(numbersActivity, 0, words);
+public class WordAdapter extends ArrayAdapter<Word> {
+
+    private int mColorResourceId;
+
+    public WordAdapter(Context context, ArrayList<Word> words, int colorResourceId) {
+        super(context, 0, words);
+        mColorResourceId = colorResourceId;
+
     }
+
+
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
@@ -20,7 +31,10 @@ public class WordAdapter extends ArrayAdapter<Word>  {
         if (listItemView == null) {
             listItemView = LayoutInflater.from(getContext()).inflate(
                     R.layout.list_item, parent, false);
+
         }
+
+
         // Get the {@link AndroidFlavor} object located at this position in the list
         Word currentWord = getItem(position);
 
@@ -37,13 +51,28 @@ public class WordAdapter extends ArrayAdapter<Word>  {
         miwokTextView.setText(currentWord.getMiwokTranslation());
 
         // Find the ImageView in the list_item.xml layout with the ID list_item_icon
-//        ImageView iconView = (ImageView) listItemView.findViewById(R.id.list_item_icon);
-        // Get the image resource ID from the current AndroidFlavor object and
-        // set the image to iconView
-//        iconView.setImageResource(currentWord.getImageResourceId());
-        // Return the whole list item layout (containing 2 TextViews and an ImageView)
-        // so that it can be shown in the ListView
-        return listItemView;
+        ImageView iconView = (ImageView) listItemView.findViewById(R.id.imageView);
+        // Check if an image is provided for this word or not
+        if (currentWord.hasImage()) {
+            // If an image is available, display the provided image based on the resource ID
+            iconView.setImageResource(currentWord.getImageResourceId());
+            // Make sure the view is visible
+            iconView.setVisibility(View.VISIBLE);
+        } else {
+            // Otherwise hide the ImageView (set visibility to GONE)
+            iconView.setVisibility(View.GONE);
+        }
 
+        // Set the theme color for the list item
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        // Find the color that the resource ID maps to
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        // Set the background color of the text container View
+        textContainer.setBackgroundColor(color);
+
+        // Return the whole list item layout (containing 2 TextViews) so that it can be shown in
+        // the ListView.
+        return listItemView;
     }
 }
+
